214e3aa72177bc2c16c3dcb45ca199c52a3f1cbe
Lejun Min <code@lejun.site>
2026-04-28 01:22:25 -0700

    remove unneeded

--- home/.config/fish/functions/avim.fish
+++
@@ -1,4 +0,0 @@
-function avim --wraps='NVIM_APPNAME=astro nvim' --description 'alias avim=NVIM_APPNAME=astro nvim'
-  NVIM_APPNAME=astro nvim $argv
-        
-end
--- home/.config/fish/functions/ranger-cd.fish
+++
@@ -1,7 +0,0 @@
-function ranger-cd
-    set dir (mktemp -t ranger_cd.XXX)
-    ranger --choosedir=$dir
-    cd (cat $dir) $argv
-    command rm $dir
-    commandline -f repaint
-end
